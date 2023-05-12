222ae2b0c704b9e12990910e75048032057d7186
Niels Kesting <78323677+NielsKesting@users.noreply.github.com>
2023-05-12 11:53:09 +0200

    Added Styling to recipeCards

--- src/components/ui/recipeCard.jsx
+++ src/components/ui/recipeCard.jsx
@@ -1,15 +1,31 @@
-import "./recipeCard.css";
+import "./RecipeCard.css";
 
 export const RecipeCard = (recipe) => {
   console.log(recipe);
-  // return (
-  //   <>
-  //     <img src={recipe.image} className="recipeCardPicture"></img>
-  //     <div className="recipeCardInfoBox">
-  //       <p>{recipe.mealType}</p>
-  //       <h3>{recipe.label}</h3>
-  //       <p>{recipe.dishType}</p>
-  //     </div>
-  //   </>
-  // );
+  const cardInfo = recipe.recipe.recipe;
+
+  const veganVegetarian = () => {
+    if (cardInfo.healthLabels.includes("Vegan")) {
+      return "Vegan";
+    } else if (cardInfo.healthLabels.includes("Vegetarian")) {
+      return "Vegetarian";
+    }
+  };
+
+  return (
+    <>
+      <div className="recipeCard">
+        <img src={cardInfo.image} className="recipeCardPicture"></img>
+        <div className="recipeCardInfoBox">
+          <p>{cardInfo.mealType}</p>
+          <h2>{cardInfo.label}</h2>
+          <p>{veganVegetarian()}</p>
+          <p>{cardInfo.dietLabels}</p>
+          <p>Dish: {cardInfo.dishType}</p>
+          <p>Cautions</p>
+          <p>{cardInfo.cautions}</p>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,24 +1,24 @@
 // import { Center, Heading } from "@chakra-ui/react";
-// import "./RecipeListPage.css";
+import "./RecipeListPage.css";
 import { data } from "../utils/data";
-import { RecipeCard } from "../components/ui/recipeCard.jsx";
+import { RecipeCard } from "../components/ui/RecipeCard.jsx";
 
 export function RecipeListPage() {
-  // const recipeList = data.hits[0];
-  // return <RecipeCard recipe={recipeList} />;
+  const recipeList = data.hits[0];
+  return <RecipeCard recipe={recipeList} />;
 
-  const recipeList = data.hits.map((recipe) => {
-    const dish = recipe.recipe;
-    // console.log(dish);
-    return { dish };
-  });
-  console.log(recipeList);
+  // const recipeList = data.hits.map((recipe) => {
+  //   const dish = recipe.recipe;
+  //   // console.log(dish);
+  //   return { dish };
+  // });
+  // console.log(recipeList);
 
-  recipeList.forEach((recipe) => {
-    return (
-      <div>
-        <RecipeCard recipe={recipe} />
-      </div>
-    );
-  });
+  // recipeList.forEach((recipe) => {
+  //   return (
+  //     <div>
+  //       <RecipeCard recipe={recipe} />
+  //     </div>
+  //   );
+  // });
 }
