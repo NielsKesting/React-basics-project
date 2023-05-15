b106c59012ced4aa761e083c526590353ab78bb2
Niels Kesting <niels.kesting@gmail.com>
2023-05-15 10:09:45 +0200

    Added loop for recipe cards

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,24 +1,11 @@
-// import { Center, Heading } from "@chakra-ui/react";
-import "./RecipeListPage.css";
+import "./RecipeListPage.css"
 import { data } from "../utils/data";
 import { RecipeCard } from "../components/ui/RecipeCard.jsx";
 
 export function RecipeListPage() {
-  const recipeList = data.hits[0];
-  return <RecipeCard recipe={recipeList} />;
-
-  // const recipeList = data.hits.map((recipe) => {
-  //   const dish = recipe.recipe;
-  //   // console.log(dish);
-  //   return { dish };
-  // });
-  // console.log(recipeList);
-
-  // recipeList.forEach((recipe) => {
-  //   return (
-  //     <div>
-  //       <RecipeCard recipe={recipe} />
-  //     </div>
-  //   );
-  // });
+  const recipeList = data.hits;
+  let x = recipeList.map((element) => {
+      return <RecipeCard recipe={element} />
+  });
+  return x;
 }
