b606d97803a9099456f22ecc580bf24d2963391b
Niels Kesting <78323677+NielsKesting@users.noreply.github.com>
2023-08-01 17:44:37 +0200

    RecipeCard and Recipepage are done

--- src/components/ui/RecipePage.jsx
+++ src/components/ui/RecipePage.jsx
@@ -3,6 +3,19 @@ import "./RecipePageStyle/RecipePage.css";
 export const RecipePage = (recipe) => {
   const info = recipe.recipe.recipe;
 
+  const ingredients = () => {
+    let ingredientList = info.ingredientLines.map((ingredient) =>{
+      return <li>{ingredient}</li>
+    })
+
+    return (
+      <>
+      <h2>Ingredients:</h2>
+      <ul className="ingredientList">{ingredientList}</ul>
+      </>
+    )
+  }
+
   const healthLabels = () => {
     let healthLabelList = info.healthLabels.map((label) => {
       return <li>{label}</li>;
@@ -10,8 +23,10 @@ export const RecipePage = (recipe) => {
 
     return (
       <>
-        <p>Health labels:</p>
-        <ul className="healthLabelList">{healthLabelList}</ul>
+        <div className="healthLabels">
+          <p>Health labels:</p>
+          <ul className="healthLabelList">{healthLabelList}</ul>
+        </div>
       </>
     );
   };
@@ -22,7 +37,14 @@ export const RecipePage = (recipe) => {
         return <li>{labels}</li>;
       });
 
-      return <ul className="dietLabels">{labelList}</ul>;
+      return (
+        <>
+          <div className="diet">
+            <p>Diet:</p>
+            <ul className="dietLabels">{labelList}</ul>
+            </div>
+        </>
+      )
     }
   };
 
@@ -34,51 +56,64 @@ export const RecipePage = (recipe) => {
 
       return (
         <>
-          <p>Cautions:</p>
-          <ul className="cautionList">{cautionList}</ul>
+          <div className="cautions">
+            <p>Cautions:</p>
+            <ul className="cautionList">{cautionList}</ul>
+          </div>
         </>
       );
     }
   };
 
-  // const totalNutrients = () => {
-  //   const filters = ["ENERC_KCAL", "CHOCDF", "PROCNT", "FAT", "CHOLE", "NA"];
-  //   const totalNutrients = info.totalNutrients;
+  const totalNutrients = () => {
+    // const nutrients = ["ENERC_KCAL", "CHOCDF", "PROCNT", "FAT", "CHOLE", "NA"];
+    const calories = Math.trunc(info.totalNutrients.ENERC_KCAL.quantity)
+    const carbs = Math.trunc(info.totalNutrients.CHOCDF.quantity)
+    const protein = Math.trunc(info.totalNutrients.PROCNT.quantity)
+    const fat =  Math.trunc(info.totalNutrients.FAT.quantity)
+    const cholesterol = Math.trunc(info.totalNutrients.CHOLE.quantity)
+    const sodium = Math.trunc(info.totalNutrients.NA.quantity)
 
-  //   const neededNutrients = (filters, nutrients) => {
-  //     filters.forEach((filter) => {
-  //       if (nutrients === filter) {
-  //         return totalNutrients.filter;
-  //       }
-  //     });
-  //   };
+    return(
+      <>
+        <div className="nutrients">
+          <p>Total nutrients:</p>
+          <ul className="nutrientList">
+            <li>{calories} CALORIES</li>
+            <li>{carbs} g CARBS</li>
+            <li>{protein} g PROTEIN</li>
+            <li>{fat} g FAT</li>
+            <li>{cholesterol} mg CHOLESTEROL</li>
+            <li>{sodium} mg SODIUM</li>
+          </ul>
+        </div>
+      </>
+    )
 
-  //   console.log(neededNutrients(filters, totalNutrients));
-  // };
-  // totalNutrients();
+  };
 
   return (
     <>
       <div className="recipePage">
         <div className="recipePageBanner">
-          <button type="button" id="backBtn">backbutton</button>
+          <button type="button" id="backBtn">Back</button>
         </div>
         <img src={info.image} className="recipePagePicture"></img>
-        <div className="recipePageInfo">
-          <div className="recipePageInfoLeft">
-            <p>{info.mealType}</p>
-            <h2>{info.label}</h2>
-          </div>
-          <div className="recipePageInfoRight">
-            {healthLabels()}
-            {dietLabels()}
-            {cautions()}
-            <p>Total nutrients:</p>
-            {/* {totalNutrients()} */}
-          </div>
+        <div className="recipePageInfoLeft">
+          <p>{info.mealType}</p>
+          <h2>{info.label}</h2>
+          <p>Total cooking time: {info.totalTime} minutes</p>
+          <p>Servings: {info.yield}</p>
+          {ingredients()}
         </div>
+        <div className="recipePageInfoRight">
+          {healthLabels()}
+          {dietLabels()}
+          {cautions()}
+          {totalNutrients()}
+        </div>
+        <div className="recipePageFooter"></div>
       </div>
     </>
   );
 };
-
